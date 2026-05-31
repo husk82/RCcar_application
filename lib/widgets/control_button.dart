4876e7b54c1ec1cb7370f25f22c19081d02c8205
Aashish Tako <takoaashish01@gmail.com>
2026-05-31 00:05:22 -0500

    Refined command logic

--- lib/widgets/control_button.dart
+++ lib/widgets/control_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class ControlButton extends StatelessWidget
+class ControlButton extends StatefulWidget
 {
   final IconData icon;
   final String command;
@@ -14,19 +15,59 @@ class ControlButton extends StatelessWidget
   });
 
   @override
+  State<ControlButton> createState() => _ControlButtonState();
+
+}
+
+class _ControlButtonState extends State<ControlButton>
+{
+  double opacity = 0.5;
+  Timer? _holdTimer;
+
+  @override
   Widget build(BuildContext context) 
   {
-     return Listener(
-      onPointerDown: (_) => sendCommand(command),
-      onPointerUp: (_) => sendCommand("STOP"),
-      onPointerCancel: (_) => sendCommand("STOP"),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          borderRadius: BorderRadius.circular(12),
+    return Listener(
+    onPointerDown: (_) 
+      {
+        setState(() => opacity = 1.0);
+        widget.sendCommand(widget.command);
+
+        _holdTimer = Timer.periodic(Duration(milliseconds: 100), (_) {
+          widget.sendCommand(widget.command);
+        });
+      },
+      onPointerUp: (_)
+      {
+        _holdTimer?.cancel();
+        _holdTimer = null;
+
+        setState(() => opacity = 0.5);
+        widget.sendCommand("S");
+      },
+      onPointerCancel: (_)
+      {
+        _holdTimer?.cancel();
+        _holdTimer = null;
+
+        setState(() => opacity = 0.5);
+        widget.sendCommand("S");
+      },
+      child: AnimatedOpacity(
+        opacity: opacity, 
+        duration: const Duration(microseconds: 120),
+        child: Container(
+            padding:  const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Icon(
+              widget.icon,
+              size: 20,
+              color: Colors.white,
+            ),
         ),
-        child: Icon(icon, size: 30),
       ),
     );
   }
